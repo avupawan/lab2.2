06e43365ec5c4bb5c9afe32bdaec1e60af691a45
Ubuntu <avupawanreddy@hotmail.com>
2019-05-18 13:03:37 +0000

    DevOps day 1

--- demo.java
+++ demo.java
@@ -1,11 +1,9 @@
 class first {
 	public static void main (String[] arguments) {
-<<<<<<< HEAD
+
 		System.out.println(Day1);
 		System.out.println("modified for DevOps demo");
-=======
 		System.out.println("let's have a hands on DevOps");
-		System.out.println("let's have a hands on DevOps");
->>>>>>> f0486ffd2dd03721b08544975523dbcf1cefd272
+
 	}
 }
